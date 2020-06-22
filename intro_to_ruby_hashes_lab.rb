6ac835fe0a2fe1220f5342aa14d59f6336b7e942
Lesly Juarez <leslyj2@gmail.com>
2020-06-22 18:12:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,10 +37,10 @@ end			{
 		},
 		:rent_in_dollars =>
 		{
-			:one_piece_owned => 25,
-			:two_pieces_owned => 50,
-			:three_pieces_owned => 100,
-			:four_pieces_owned => 200
+			:one_piece_owned => "25",
+			:two_pieces_owned => "50",
+			:three_pieces_owned => "100",
+			:four_pieces_owned => "200"
 		}
 		}
 	}#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
@@ -56,11 +56,11 @@ def monopoly_with_fourth_tier
 		{
 			:reading_railroad =>
 			{
-				"mortgage_value"=>"$100"
+				"mortgage_value"=> "$100"
 			},
 			:pennsylvania_railroad =>
 			{
-				"mortgage_value"=>"$200"
+				"mortgage_value"=> "$200"
 			},
 			:b_and_o_railroad =>
 			{
@@ -73,10 +73,10 @@ def monopoly_with_fourth_tier
 		},
 		:rent_in_dollars =>
 		{
-			:one_piece_owned =>25,
-			:two_pieces_owned =>50,
-			:three_pieces_owned =>100,
-			:four_pieces_owned =>200
+			:one_piece_owned => "25",
+			:two_pieces_owned => "50",
+			:three_pieces_owned => "100",
+			:four_pieces_owned => "200"
 		}
 		}
 	}#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
